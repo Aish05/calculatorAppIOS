18b26bb45fe710482a871395ae7487115ad35021
Aishwarya Iyer <aradhakrishnaniye@deloitte.com>
2022-10-06 12:54:48 +0530

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Radhakrishnan Iyer, Aishwarya on 06/10/22.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
